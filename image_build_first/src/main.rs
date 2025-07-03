29faa95978c7974de6b81afebf2468d3f06adea3
gesi <15733996517@163.com>
2025-07-03 09:40:06 +0800

    第二周任务完成，运行了11654.690114188s

--- image_build_first/src/main.rs
+++ image_build_first/src/main.rs
@@ -35,7 +35,7 @@ fn main() -> std::io::Result<()> {
     // eprintln!("Current dir: {:?}\n", std::env::current_dir().unwrap());
     let start = Instant::now();
 
-    match 8 {
+    match 9 {
         1 => bouncing_spheres(),
         2 => checker_spheres(),
         3 => earth(),
@@ -44,7 +44,8 @@ fn main() -> std::io::Result<()> {
         6 => simple_light(),
         7 => cornell_box(),
         8 => cornell_smoke(),
-        _ => bouncing_spheres(),
+        9 => final_scene(800, 10000, 40),
+        _ => final_scene(400, 250, 4),
     }?;
 
     let duration = start.elapsed();
@@ -383,6 +384,81 @@ fn cornell_smoke() -> Result<(), std::io::Error> {
     cam.background = Color::new(0.0, 0.0, 0.0);
     cam.vfov = 40.0;
     cam.lookfrom = Point3::new(278.0, 278.0, -800.0);
+    cam.lookat = Point3::new(278.0, 278.0, 0.0);
+    cam.vup = Vec3::new(0.0, 1.0, 0.0);
+
+    cam.defocus_angle = 0.0;
+
+    let stdout = stdout();                      // 获取 stdout 句柄
+    let writer = BufWriter::new(stdout); 
+    cam.initialize();
+    cam.render(world, writer)?; 
+    Ok(())
+}
+
+fn final_scene(image_width : usize, sample_per_pixel : usize, max_depth : usize) -> Result<(), std::io::Error> {
+    let mut boxes1 = HittableList::new();
+    let ground = Arc::new(Lambertian::new(Color::new(0.48, 0.83, 0.53)));
+
+    for i in 0..20 {
+        for j in 0..20 {
+            let w = 100.0;
+            let x0 = -1000.0 + i as f64 * w;
+            let z0 = -1000.0 + j as f64 * w;
+            let y0 = 0.0;
+            let x1 = x0 + w;
+            let y1 = rtweekend::random_double_range(1.0, 101.0);
+            let z1 = z0 + w;
+
+            boxes1.add(Quad::make_box(&Point3::new(x0, y0, z0), &Point3::new(x1, y1, z1), ground.clone()));
+        }
+    }
+
+    let mut world = HittableList::new();
+    world.add(Arc::new(BvhNode::new_from_list(&boxes1)));
+
+    let light = Arc::new(DiffuseLight::from_color(Color::new(7.0, 7.0, 7.0)));
+    world.add(Arc::new(Quad::new(Point3::new(123.0, 554.0, 147.0), Vec3::new(300.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 265.0), light)));
+
+    let center1 = Point3::new(400.0, 400.0, 200.0);
+    let center2 = center1 + Vec3::new(30.0, 0.0, 0.0);
+    let sphere_material = Arc::new(Lambertian::new(Color::new(0.7, 0.3, 0.1)));
+    world.add(Arc::new(Sphere::new_moving(center1, center2, 50.0, sphere_material)));
+
+    world.add(Arc::new(Sphere::new_stationary(Point3::new(260.0, 150.0, 45.0), 50.0, Arc::new(Dielectric::new(1.5)))));
+    world.add(Arc::new(Sphere::new_stationary(Point3::new(0.0, 150.0, 145.0), 50.0, Arc::new(Metal::new(Color::new(0.8, 0.8, 0.9), 1.0)))));
+
+    let boundary = Arc::new(Sphere::new_stationary(Point3::new(360.0, 150.0, 145.0), 70.0, Arc::new(Dielectric::new(1.5))));
+    world.add(boundary.clone());
+    world.add(Arc::new(ConstantMedium::new_with_color(boundary, 0.2, Color::new(0.2, 0.4, 0.9))));
+    let boundary = Arc::new(Sphere::new_stationary(Point3::new(0.0, 0.0, 0.0), 5000.0, Arc::new(Dielectric::new(1.5))));
+    world.add(Arc::new(ConstantMedium::new_with_color(boundary, 0.0001, Color::new(1.0, 1.0, 1.0))));
+
+    let emat = Arc::new(Lambertian::from_texture(Arc::new(ImageTexture::new("earthmap.jpg"))));
+    world.add(Arc::new(Sphere::new_stationary(Point3::new(400.0, 200.0, 400.0), 100.0, emat)));
+    let pertext = Arc::new(NoiseTexture::new(0.2));
+    world.add(Arc::new(Sphere::new_stationary(Point3::new(220.0, 280.0, 300.0), 80.0, Arc::new(Lambertian::from_texture(pertext)))));
+
+    let mut boxes2 = HittableList::new();
+    let white = Arc::new(Lambertian::new(Color::new(0.73, 0.73, 0.73)));
+    for _j in 0..1000 {
+        boxes2.add(Arc::new(Sphere::new_stationary(Point3::random_range(0.0, 165.0), 10.0, white.clone())));
+    }
+
+    world.add(Arc::new(Translate::new(Arc::new(RotateY::new(Arc::new(BvhNode::new_from_list(&boxes2)), 15.0)),Vec3::new(-100.0, 270.0, 395.0))));
+
+    let bvh_root = Arc::new(BvhNode::new_from_list(&world));
+    let world = bvh_root;
+
+    let aspect_ratio : f64 = 1.0;
+
+    //  camera
+    let mut cam = Camera::new(aspect_ratio, image_width);
+    cam.sample_per_pixel = sample_per_pixel;
+    cam.max_depth = max_depth;
+    cam.background = Color::new(0.0, 0.0, 0.0);
+    cam.vfov = 40.0;
+    cam.lookfrom = Point3::new(478.0, 278.0, -600.0);
     cam.lookat = Point3::new(278.0, 278.0, 0.0);
     cam.vup = Vec3::new(0.0, 1.0, 0.0);
 
